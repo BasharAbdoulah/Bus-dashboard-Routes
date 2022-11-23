2b28187ae42fa4c0edaefd517e488998f3df5d1b
basharAbdullah <des.basharabdulah@gmail.com>
2022-11-23 11:35:19 +0300

    show packages statistics done & fix tables error

--- src/views/Paymentwallet/Send.js
+++ src/views/Paymentwallet/Send.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 //component
-import { Row, Col, Input, Checkbox, Form, Select, Alert, Empty } from "antd";
+import { Row, Input, Form, Select, Empty } from "antd";
 import { Table } from "ant-table-extensions";
-import useFetch from "hooks/useFetch";
 import { SearchOutlined } from "@ant-design/icons";
 import axios, { Axios } from "axios";
 import { useSelector } from "react-redux";
