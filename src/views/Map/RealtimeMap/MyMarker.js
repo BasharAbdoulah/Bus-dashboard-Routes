577b067369cf7991f709e52732495f99f3b2d61c
basharAbdullah <des.basharabdulah@gmail.com>
2023-02-16 11:27:14 +0300

    Markers issue has been fixed

---
+++ src/views/Map/RealtimeMap/MyMarker.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+const MyMarker = ({ routeName, plateNumber, $hover }) => {
+  const [isShow, setIsShow] = useState(false);
+  const handleClick = () => {
+    setIsShow(!isShow);
+  };
+
+  return (
+    <div
+      className={$hover ? "circle hover" : "circle center"}
+      onClick={handleClick}
+    >
+      <div className="shadow"></div>
+      <span className="circleText center" title={routeName}>
+        {routeName}
+      </span>
+      {isShow && (
+        <div className="info-window center">
+          <span>Route number: {routeName}</span>
+          <span>Plate number: {plateNumber}</span>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MyMarker;
